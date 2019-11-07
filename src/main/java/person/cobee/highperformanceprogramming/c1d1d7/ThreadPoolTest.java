3a2c930a002ddef0a7bf93431fe5084d85f4b268
cobee <18027041863@163.com>
2019-11-07 11:23:42 +0800

    定时任务线程池测试

--- src/main/java/person/cobee/highperformanceprogramming/c1d1d7/ThreadPoolTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d1d7/ThreadPoolTest.java
@@ -13,7 +13,11 @@ public class ThreadPoolTest {
     public static void main(String[] args) throws InterruptedException {
         ThreadPoolTest threadPoolTest = new ThreadPoolTest();
 //        threadPoolTest.threadPoolTest1();
-        threadPoolTest.threadPoolTest2();
+//        threadPoolTest.threadPoolTest2();
+//        threadPoolTest.threadPoolTest3();
+//        threadPoolTest.threadPoolTest4();
+//        threadPoolTest.threadPoolTest5();
+        threadPoolTest.threadPoolTest6();
     }
 
     /**
@@ -63,6 +67,88 @@ public class ThreadPoolTest {
         System.out.println("线程池等待队列大小：" + threadPoolExecutor.getQueue().size());
         // 停止接收任务，并且关闭线程池
         threadPoolExecutor.shutdown();
+    }
+
+    /**
+     * 测试标准线程池的使用方法
+     * 固定线程池的使用方法，核心线程数量与最大线程数量一致，无界队列
+     */
+    private void threadPoolTest3() throws InterruptedException {
+        // Executors.newFixedThreadPool(5);
+        // 核心线程数量5，最大线程数量5，空闲线程存活时间0秒，使用无界队列，队列的容量是无限大
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 5, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        testCommon(threadPoolExecutor);
+        // 停止接收任务，并且关闭线程池
+        threadPoolExecutor.shutdown();
+    }
+
+    /**
+     * 测试标准线程池的使用方法
+     * 缓冲线程池，核心线程数0，最大线程数Integer.MAX_VALUE，空闲线程的存活时间60L，使用同步队列。
+     */
+    private void threadPoolTest4() throws InterruptedException {
+        // Executors.newCachedThreadPool();
+        // 核心线程数量0，最大线程数量Integer.MAX_VALUE，空闲线程存活时间60秒，使用同步队列。
+        // 同步队列不是真正的队列，当有任务来的时候如果有空闲的线程，则使用。如果没有会新开一个线程
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<>());
+        testCommon(threadPoolExecutor);
+        Thread.sleep(60000L);
+        System.out.println("线程池大小：" + threadPoolExecutor.getPoolSize());
+        System.out.println("线程池等待队列大小：" + threadPoolExecutor.getQueue().size());
+        // 停止接收任务，并且关闭线程池
+        threadPoolExecutor.shutdown();
+    }
+
+    /**
+     * 测试定时任务线程池的使用方法
+     * 任务只执行一次
+     */
+    private void threadPoolTest5() throws InterruptedException {
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(5);
+        System.out.println("当前时间:" + System.currentTimeMillis());
+        // 在多少秒之后执行一次
+        scheduledThreadPoolExecutor.schedule(() -> {
+            System.out.println("开始执行时间：" + System.currentTimeMillis());
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("结束执行时间：" + System.currentTimeMillis());
+        }, 1L, TimeUnit.SECONDS);
+        scheduledThreadPoolExecutor.shutdown();
+    }
+
+    /**
+     * 测试定时任务线程池的使用方法
+     * 任务周期性地执行，任务不会并发执行，会等待上一个任务执行完再执行。
+     */
+    private void threadPoolTest6() throws InterruptedException {
+//        Executors.newScheduledThreadPool(5);
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(5);
+        System.out.println("当前时间:" + System.currentTimeMillis());
+        // 在多少秒之后执行一次
+//        scheduledThreadPoolExecutor.scheduleAtFixedRate(() -> {
+//            System.out.println("开始执行时间：" + System.currentTimeMillis());
+//            try {
+//                Thread.sleep(3000L);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("结束执行时间：" + System.currentTimeMillis());
+//        }, 2L, 1L, TimeUnit.SECONDS);
+
+        // 在多少秒之后执行一次
+        scheduledThreadPoolExecutor.scheduleWithFixedDelay(() -> {
+            System.out.println("开始执行时间：" + System.currentTimeMillis());
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("结束执行时间：" + System.currentTimeMillis());
+        }, 2L, 1L, TimeUnit.SECONDS);
+//        scheduledThreadPoolExecutor.shutdown();
     }
 
 }
