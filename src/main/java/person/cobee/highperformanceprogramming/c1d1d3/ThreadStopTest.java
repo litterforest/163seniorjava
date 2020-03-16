c21fbdc7b77fb1bdc667952816734dc9cb342533
cobee <18027041863@163.com>
2020-03-16 23:22:27 +0800

    优化代码、优化注释

--- src/main/java/person/cobee/highperformanceprogramming/c1d1d3/ThreadStopTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d1d3/ThreadStopTest.java
@@ -11,6 +11,7 @@ public class ThreadStopTest {
     public static void main(String[] args) throws InterruptedException {
         CalcRunnable calcRunnable = new CalcRunnable();
         Thread thread = new Thread(calcRunnable);
+//        thread.interrupt(); // 中断不活动的线程没有任何效果
         thread.start();
         Thread.sleep(1000);
         // thread.stop(); 强制终止，线程直接销毁
