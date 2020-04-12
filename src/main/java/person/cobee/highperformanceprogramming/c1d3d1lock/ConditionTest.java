761f89e2afbab412ec18f54b4061d0f7fe0bdf5d
cobee <18027041863@163.com>
2020-04-12 17:02:18 +0800

    可重入锁和condition使用测试

--- src/main/java/person/cobee/highperformanceprogramming/c1d3d1lock/ConditionTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d3d1lock/ConditionTest.java
@@ -11,30 +11,33 @@ import java.util.concurrent.locks.ReentrantLock;
  * @since 2019-11-20
  */
 public class ConditionTest {
-
+    // 阻塞队列
     private static Lock lock = new ReentrantLock();
-    // 等待队列
+    // 等待集合
     private static Condition await = lock.newCondition();
 
     public static void main(String[] args) throws InterruptedException {
-
         Thread th = new Thread(() -> {
             lock.lock();
+            System.out.println(Thread.currentThread() + ":进入等待状态");
             try {
-                System.out.println(Thread.currentThread() + ":进入等待状态");
-                await.await();
+                await.await(); // 进入等待状态，并且释放锁。被唤醒后会重新抢锁
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally{
+                lock.unlock();
             }
             System.out.println(Thread.currentThread() + ":等待结束");
-            lock.unlock();
         });
         th.start();
         Thread.sleep(300L);
-        lock.lock();
-        System.out.println("唤醒等待线程");
-        await.signal();
-        lock.unlock();
+        try {
+            lock.lock();
+            System.out.println("唤醒等待线程");
+            await.signal();
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
--- src/main/java/person/cobee/highperformanceprogramming/c1d3d1lock/LockTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d3d1lock/LockTest.java
@@ -16,26 +16,31 @@ public class LockTest {
         // 非公平锁，可重入锁
         Lock lock = new ReentrantLock();
         lock.lock();
+        lock.lock();
+        lock.unlock(); // 锁没有完全释放，其它线程也不能获取到锁
+//        lock.unlock();
+//        lock.unlock(); // 单独调用unlock方法，会抛出监视器状态异常
         System.out.println("主线程获取锁");
         Thread th = new Thread(() -> {
             System.out.println(Thread.currentThread() + ":开始获取锁");
-            lock.lock(); // 阻塞获取锁
+//            lock.lock(); // 阻塞获取锁
 //            lock.tryLock();  // 获取锁，没有获取到就返回
 //            try {
 //                lock.tryLock(3L, TimeUnit.SECONDS);
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
-//            try {
-//                lock.lockInterruptibly();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                lock.lockInterruptibly();
+            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
             System.out.println(Thread.currentThread() + ":成功获取锁");
         });
         th.start();
         Thread.sleep(5000L);
-        System.out.println("执行线程中断");
+//        System.out.println("执行线程中断");
         th.interrupt();
     }
 
