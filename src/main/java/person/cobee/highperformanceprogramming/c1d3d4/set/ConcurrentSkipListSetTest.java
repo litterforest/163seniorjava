67b827e7673aadc62f52cafb791eeff9c3643fa4
cobee <18027041863@163.com>
2020-04-19 22:32:22 +0800

    带跳表查询功能的有顺序的集合。通过跳表索引增加查询速度。

--- src/main/java/person/cobee/highperformanceprogramming/c1d3d4/set/ConcurrentSkipListSetTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d3d4/set/ConcurrentSkipListSetTest.java
@@ -3,7 +3,7 @@ package person.cobee.highperformanceprogramming.c1d3d4.set;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
- * 功能描述
+ * 高并发线程安全、不重复，有顺序、高效查找的集合
  *
  * @author cobee
  * @since 2019-11-28
