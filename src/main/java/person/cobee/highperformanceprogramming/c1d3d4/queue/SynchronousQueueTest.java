3e356802a7aac9e97aca2580f8fda963fb2e6534
cobee <18027041863@163.com>
2020-04-19 23:09:32 +0800

    同步队列测试

--- src/main/java/person/cobee/highperformanceprogramming/c1d3d4/queue/SynchronousQueueTest.java
+++ src/main/java/person/cobee/highperformanceprogramming/c1d3d4/queue/SynchronousQueueTest.java
@@ -3,7 +3,8 @@ package person.cobee.highperformanceprogramming.c1d3d4.queue;
 import java.util.concurrent.SynchronousQueue;
 
 /**
- * 同步队列，是阻塞的，里面不放置元素，手把手的交换数据
+ * 同步队列，是阻塞的，里面不放置元素，手把手的交换数据，并且是在不同元素之间的操作
+ * 可用于做线程同步
  *
  * @author cobee
  * @since 2019-11-28
@@ -12,16 +13,22 @@ public class SynchronousQueueTest {
 
     public static void main(String[] args) throws InterruptedException {
         SynchronousQueue<String> q = new SynchronousQueue<>();
-        new Thread(() -> {
-            System.out.println("数据入队。。。");
-            try {
-                q.put("a");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("数据入队完毕。。。");
-        }).start();
-        System.out.println(q.take());
+        q.put("a");
+//        q.put("b");
+//        q.put("c");
+        q.take();
+//        new Thread(() -> {
+//            System.out.println("数据入队。。。");
+//            try {
+//                Thread.sleep(3000L);
+//                q.put("a");
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("数据入队完毕。。。");
+//        }).start();
+//        System.out.println("主线程开始获取数据");
+//        System.out.println(q.take());
     }
 
 }
